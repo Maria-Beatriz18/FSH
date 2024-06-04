2e9b7dd02447cf26bb5384df7eaa16e4c6156cb9
WIDANET <willianlps99@outlook.com>
2024-06-04 16:54:02 -0300

    update courses

--- src/paginas/Cursos/graduacao.js
+++ src/paginas/Cursos/graduacao.js
@@ -1,15 +1,53 @@
+import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
+import Curso from "../../Components/Lista_cursos"; // Importe o componente Curso
 
+const cursos = [
+  {
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiXLC37AdcvJNR66QuLcBU2yYI3TSIU9Csgw&s",
+    titulo: "Ciências Contábeis",
+    texto: "Aprenda a criar interfaces web interativas com React.",
+    link: "https://wa.link/4m0z6k",
+  },
+  {
+    imagem: "https://profissoes.vagas.com.br/wp-content/uploads/2022/11/areas-de-atuacao-na-administracao.jpg",
+    titulo: "Administração",
+    texto: "Domine a linguagem base da web e crie scripts poderosos.",
+    link: "https://wa.link/4m0z6k",
+  },
+  {
+    imagem: "https://s2-g1.glbimg.com/xUmLXNVrkLIR8MBv89A6EhFMoew=/0x0:1600x900/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/v/e/QAQIQjQYaYodxf7Bv5ww/foto-1.jpg",
+    titulo: "Direito",
+    texto: "Domine a linguagem base da web e crie scripts poderosos.",
+    link: "https://wa.link/4m0z6k",
+  },
+  {
+    imagem: "https://www.vestibulandoweb.com.br/wp-content/uploads/2021/06/profissao-servico-social.jpg.webp",
+    titulo: "Serviço Social",
+    texto: "Domine a linguagem base da web e crie scripts poderosos.",
+    link: "https://wa.link/4m0z6k",
+  },
+  
+  
+  // Adicione mais cursos aqui
+];
 
-function Graduacao (){
-
-    return(
-     <div className="App">
-      <Navbar/>
+function Tec() {
+  return (
+    <div className="App">
+      <Navbar />
+      <section className="cursos-container">
+        <h2 className="titulo-cursos">GRADUAÇÃO</h2>
+        <div className="lista-cursos">
+          {cursos.map((curso) => (
+            <Curso key={curso.titulo} {...curso} /> 
+          ))}
+        </div>
+      </section>
       <Footer />
     </div>
-    )
+  );
 }
 
-export default Graduacao;
+export default Tec;
--- src/paginas/Cursos/pos.js
+++ src/paginas/Cursos/pos.js
@@ -1,15 +1,45 @@
+import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
+import Curso from "../../Components/Lista_cursos"; // Importe o componente Curso
 
+const cursos = [
+  {
+    imagem: "https://fia.com.br/wp-content/uploads/2022/06/gestao-de-pessoas-o-que-e-importancia-como-desenvolver-1280x720.jpg",
+    titulo: "Gestão de pessoas e liderança",
+    texto: "Aprenda a criar interfaces web interativas com React.",
+    link: "https://wa.link/4m0z6k",
+  },
+  {
+    imagem: "https://www.insper.edu.br/wp-content/uploads/2023/11/01_CAPA_Qual_e_a_diferenca_entre_-contabilidade_controladoria.jpg",
+    titulo: "Contabilidade tributária",
+    texto: "Domine a linguagem base da web e crie scripts poderosos.",
+    link: "https://wa.link/4m0z6k",
+  },
+  {
+    imagem: "https://setting.com.br/wp-content/uploads/2018/10/gestao-financeira.jpeg",
+    titulo: "Gestão de finanças e controladoria",
+    texto: "Domine a linguagem base da web e crie scripts poderosos.",
+    link: "https://wa.link/4m0z6k",
+  },
+  // Adicione mais cursos aqui
+];
 
-function Pos (){
-
-    return(
-     <div className="App">
-      <Navbar/>
+function Tec() {
+  return (
+    <div className="App">
+      <Navbar />
+      <section className="cursos-container">
+        <h2 className="titulo-cursos">PÓS- GRADUAÇÃO</h2>
+        <div className="lista-cursos">
+          {cursos.map((curso) => (
+            <Curso key={curso.titulo} {...curso} /> 
+          ))}
+        </div>
+      </section>
       <Footer />
     </div>
-    )
+  );
 }
 
-export default Pos;
+export default Tec;
