8eeb28849e5c3e5eaf1633621350691a42b1fa35
WIDANET <willianlps99@outlook.com>
2024-06-04 15:00:04 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./paginas/Cursos/tec.module.css"
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
--- src/paginas/Cursos/tec.js
+++ src/paginas/Cursos/tec.js
@@ -1,5 +1,6 @@
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
+import "./tec.module.css"
 
 
 function Tec (){
@@ -7,6 +8,20 @@ function Tec (){
     return(
      <div className="App">
       <Navbar/>
+      <section>
+        <div className="container-tec"> {/* Container principal */}
+          <div className="image-curso">
+            <img src="https://www.imagemnl.com/" alt="Imagem do curso" /> {/* Insira a URL da imagem */}
+          </div>
+          <div className="texto-curso">
+            <h2>Título do Curso</h2> {/* Altere para o título real do curso */}
+            <p>
+              Breve descrição do curso, detalhando os principais pontos abordados.
+            </p>
+          </div>
+          <button className="btn-curso">Saiba Mais</button> {/* Altere o texto do botão */}
+        </div>
+      </section>
       <Footer />
     </div>
     )
